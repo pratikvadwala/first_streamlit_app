57b40f71b39a078e208ba032aa918e12c4bbe1fc
pratikvadwala <pratik.vadwala@gmail.com>
2023-07-13 09:51:38 +1000

    API integration

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 # https://pratik-firstapp.streamlit.app/
 streamlit.title('My Parents New Healthy Diner')
@@ -21,3 +22,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+# New section to display fruityvice api response
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
