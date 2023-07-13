1fac8ba377ab594d4254a1ab46136820301369ec
pratikvadwala <pratik.vadwala@gmail.com>
2023-07-13 10:03:48 +1000

    text input to get user feedback

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,10 @@ streamlit.dataframe(fruits_to_show)
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice = streamlit.text_imput('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Take the json version of the response and normalise it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
