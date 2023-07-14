4fa4fac56818c82f2e1027cac691c39729e467b2
pratikvadwala <pratik.vadwala@gmail.com>
2023-07-15 04:57:45 +1000

    Created function

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,11 @@ import streamlit
 
 from urllib.error import URLError
 
+def get_fruityvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalised = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalised
+
 # https://pratik-firstapp.streamlit.app/
 streamlit.title('My Parents New Healthy Diner')
 
@@ -33,13 +38,9 @@ try:
   if not fruit_choice:
     streamlit.error('Please select a fruit to get information.')
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    
-    # Take the json version of the response and normalise it 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
+    fruityvice_data = get_fruityvice_data(fruit_choice)
     # Output it as a table on the page
-    streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(fruityvice_data)
 
 except URLError as e: 
   streamlit.error()
